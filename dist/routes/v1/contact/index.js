10b0e9b01d699d3fd692fefa3d780b195c1d0a6d
Dan Tovbein <dtovbein@gmail.com>
2022-01-26 08:10:14 -0300

    Update build

--- dist/routes/v1/contact/index.js
+++ dist/routes/v1/contact/index.js
@@ -13,20 +13,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-// import { IUser } from 'greenpeace';
 const middlewares_1 = require("../../../middlewares");
 // import { create, findAll } from '../user/controller';
 const api_client_1 = require("@hubspot/api-client");
 const axios_1 = __importDefault(require("axios"));
-// const hubspotClient = new hubspot.Client({ apiKey: YOUR_API_KEY })
 const router = express_1.Router();
 const hubspotClient = new api_client_1.Client({ apiKey: 'fcffbf78-18c5-40f0-a06f-5efd732f4b97' });
-// https://legacydocs.hubspot.com/docs/methods/contacts/update_contact
 router.get('/', [middlewares_1.requestWrapper((req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const contacts = yield axios_1.default({
             baseURL: 'https://api.hubapi.com/contacts/v1/lists/all/contacts/all',
             params: {
-                hapikey: 'fcffbf78-18c5-40f0-a06f-5efd732f4b97',
+                hapikey: process.env.HUBSPOT_API_KEY,
             },
         });
         res
@@ -37,7 +34,7 @@ router.get('/email/:email', [middlewares_1.requestWrapper((req, res) => __awaite
         const contact = yield axios_1.default({
             baseURL: `https://api.hubapi.com/contacts/v1/contact/email/${req.params.email}/profile`,
             params: {
-                hapikey: 'fcffbf78-18c5-40f0-a06f-5efd732f4b97'
+                hapikey: process.env.HUBSPOT_API_KEY,
             },
         });
         res
@@ -45,15 +42,11 @@ router.get('/email/:email', [middlewares_1.requestWrapper((req, res) => __awaite
             .json(contact.data.properties);
     }))]);
 router.post('/', [middlewares_1.requestWrapper((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-        // const body = req.body as any;
-        // const contact = await hubspotClient.crm.contacts.basicApi.create({
-        //   properties: req.body as any,
-        // });
         const contact = yield axios_1.default({
             baseURL: `https://api.hubapi.com/contacts/v1/contact`,
             method: 'POST',
             params: {
-                hapikey: 'fcffbf78-18c5-40f0-a06f-5efd732f4b97'
+                hapikey: process.env.HUBSPOT_API_KEY,
             },
             data: {
                 properties: Object.keys(req.body).map((key) => ({
