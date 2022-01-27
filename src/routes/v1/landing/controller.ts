a90761cc31d1d148ef6b713f4ce341fd092d8055
Dan Tovbein <dtovbein@gmail.com>
2022-01-27 19:55:47 -0300

    Move landin data to separate files

--- src/routes/v1/landing/controller.ts
+++ src/routes/v1/landing/controller.ts
@@ -2,13 +2,11 @@ import path from 'path';
 import fs from 'fs';
 import YAML from 'yaml';
 
-export const getAll = async (): Promise<any> => {
-  const result = await YAML.parse(fs.readFileSync(`${path.resolve('src')}/data/landings.yaml`, 'utf-8'));
-  return Promise.resolve(result || []);
-}
-
 export const getByName = async (name: string): Promise<any> => {
-  const result = await YAML.parse(fs.readFileSync(`${path.resolve('src')}/data/landings.yaml`, 'utf-8'));
-  const landing = result.filter((l: any) => l.name === name);
-  return Promise.resolve(landing.length ? landing[0] : {});
+  try {
+    const result = await YAML.parse(fs.readFileSync(`${path.resolve('src')}/data/landings/${name}.yaml`, 'utf-8'));
+    return Promise.resolve(result);
+  } catch(error: any) {
+    return Promise.resolve(null);
+  }
 }
--- src/routes/v1/landing/index.ts
+++ src/routes/v1/landing/index.ts
@@ -1,23 +1,22 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { IRequestError } from 'greenpeace';
 import { authWrapper } from '../../../auth';
 import { requestWrapper } from '../../../middlewares';
-import { getAll, getByName } from './controller';
+import { getByName } from './controller';
 
 const router = Router();
 
-router.get('/', [authWrapper, requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
-  const result = await getAll();
-  res
-    .status(200)
-    .json(result);  
-})]);
-
 router.get('/:name', [authWrapper, requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
   const result = await getByName(req.params.name);
-  res
-    .status(200)
-    .json(result);  
+
+  if(result) {
+    res
+      .status(200)
+      .json(result);  
+  } else {
+    res
+      .status(404)
+      .json({});
+  }
 })]);
 
 export default router;
