f09739070d08e665bb40ca2fc5120404dc07ce16
Dan Tovbein <dtovbein@gmail.com>
2022-01-26 21:57:57 -0300

    Implement auth

---
+++ src/auth.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import { requestWrapper } from "./middlewares";
+import { IRequestError } from 'greenpeace';
+
+export const authWrapper: any = requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
+  if(!req.headers['x-greenlab-app']) {
+    res
+      .status(403)
+      .json({
+        status: 403,
+        errorMessage: 'Forbbiden',
+      } as IRequestError);
+  } else {
+    next();
+  }
+});
