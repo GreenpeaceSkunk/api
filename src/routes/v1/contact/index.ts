2a1c6a0cdd86aea4c9b3320125e9ef31d249024d
Dan Tovbein <dtovbein@gmail.com>
2022-01-26 21:58:22 -0300

    Implement controller to Contact

--- src/routes/v1/contact/index.ts
+++ src/routes/v1/contact/index.ts
@@ -1,59 +1,75 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import { IRequestError } from 'greenpeace';
+import { authWrapper } from '../../../auth';
 import { requestWrapper } from '../../../middlewares';
-import axios from 'axios';
+import { createOne, findByEmail, getAll, updateOne } from './controler';
 
 const router = Router();
 
-router.get('/', [requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
-  const result = await axios({
-    baseURL: `${process.env.HUBSPOT_API_URL}/contacts/v1/lists/all/contacts/all`,
-    params: {
-      hapikey: process.env.HUBSPOT_API_KEY,
-    },
-  });
-
+router.get('/', [authWrapper, requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
+  const result = await getAll();
   res
     .status(200)
     .json(result.data.contacts.map((contact: any) => contact.properties));  
 })]);
 
-router.get('/email/:email', [requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
-  const result = await axios({
-    baseURL: `${process.env.HUBSPOT_API_URL}/contacts/v1/contact/email/${req.params.email}/profile`,
-    params: {
-      hapikey: process.env.HUBSPOT_API_KEY,
-    },
-  });
+router.get('/email/:email', [authWrapper, async (req: Request, res: Response, next: NextFunction) => {
+  const result = await findByEmail(req.params.email);
+  if(result) {
+    res
+      .status(200)
+      .json(
+        Object
+          .keys(result.data.properties)
+          .reduce((a: any, b: string) => ({ ...a, [`${b}`]: result.data.properties[b].value }), {}))
+  } else {
+    res
+      .status(404)
+      .json({
+        status: 404,
+        errorMessage: 'User does not exist.',
+      } as IRequestError);
+  }
+}]);
 
-  res
-    .status(200)
-    .json(
-      Object
-        .keys(result.data.properties)
-        .reduce((a: any, b: string) => ({ ...a, [`${b}`]: result.data.properties[b].value }), {}))
-})]);
+router.post('/', [authWrapper, async (req: Request, res: Response, next: NextFunction) => {
+  console.log("x-greenlab-app", req.headers['x-greenlab-app']);
+  const result = await createOne(req.body);
+  if(result) {
+    res
+      .status(201)
+      .json({
+        id: result.data.vid,
+        ...req.body,
+      });
+  } else {
+    res
+      .status(500)
+      .json({
+        status: 500,
+        errorMessage: 'User cannot be created or maybe exists.',
+      } as IRequestError);
+  }
+}]);
 
-router.post('/', [requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
-  const result = await axios({
-    baseURL: `${process.env.HUBSPOT_API_URL}/contacts/v1/contact`,
-    method: 'POST',
-    params: {
-      hapikey: process.env.HUBSPOT_API_KEY,
-    },
-    data: {
-      properties: Object.keys(req.body).map((key: string) => ({
-        property: `${key}`,
-        value: req.body[key],
-      })),
-    },
-  });
-
-  res
-    .status(201)
-    .json({
-      id: result.data.vid,
-      ...req.body,
-    });
-})]);
+router.post('/email/:email', [authWrapper, async (req: Request, res: Response, next: NextFunction) => {
+  const result = await updateOne(req.params.email, req.body);
+  console.log(result)
+  if(result) {
+    res
+      .status(201)
+      .json({
+        id: result.data.vid,
+        ...req.body,
+      });
+  } else {
+    res
+      .status(404)
+      .json({
+        status: 204,
+        errorMessage: 'User does not exist.',
+      } as IRequestError);
+  }
+}]);
 
 export default router;
