c9717b45ac0c77b7bb85bbbee0ee30024891afe6
Dan Tovbein <dtovbein@gmail.com>
2022-01-28 16:25:03 -0300

    Build code

---
+++ dist/routes/v1/application/controller.js
@@ -0,0 +1,29 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getByName = void 0;
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+const yaml_1 = __importDefault(require("yaml"));
+const getByName = (name) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const result = yield yaml_1.default.parse(fs_1.default.readFileSync(`${path_1.default.resolve('src')}/data/application/${name}.yaml`, 'utf-8'));
+        return Promise.resolve(result);
+    }
+    catch (error) {
+        return Promise.resolve(null);
+    }
+});
+exports.getByName = getByName;
+//# sourceMappingURL=controller.js.map
--- dist/routes/v1/index.js
+++ dist/routes/v1/index.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const hubspot_1 = __importDefault(require("./hubspot"));
-const landing_1 = __importDefault(require("./landing"));
+const application_1 = __importDefault(require("./application"));
 const router = express_1.Router();
 router.use('/hubspot', hubspot_1.default);
-router.use('/landing', landing_1.default);
+router.use('/application', application_1.default);
 exports.default = router;
 //# sourceMappingURL=index.js.map
