6230b98927892cd112d302b670976a13f66dba75
Dan Tovbein <dtovbein@gmail.com>
2022-01-31 19:06:37 -0300

    Split coupon file into settings and content

--- src/routes/v1/application/controller.ts
+++ src/routes/v1/application/controller.ts
@@ -2,10 +2,11 @@ import path from 'path';
 import fs from 'fs';
 import YAML from 'yaml';
 
-export const getCouponByName = async (name: string): Promise<any> => {
+export const getCouponByName = async (name: string, environment: string): Promise<any> => {
   try {
-    const result = await YAML.parse(fs.readFileSync(`${path.resolve('src')}/data/application/coupon/${name}.yaml`, 'utf-8'));
-    return Promise.resolve(result);
+    const content = await YAML.parse(fs.readFileSync(`${path.resolve('src')}/data/application/coupon/${name}/content.yaml`, 'utf-8'));
+    const settings = await YAML.parse(fs.readFileSync(`${path.resolve('src')}/data/application/coupon/${name}/settings.yaml`, 'utf-8'));
+    return Promise.resolve({...{content: content.data}, ...{settings: settings.data[environment]}});
   } catch(error: any) {
     return Promise.resolve(null);
   }
--- src/routes/v1/application/index.ts
+++ src/routes/v1/application/index.ts
@@ -1,12 +1,11 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { authWrapper } from '../../../auth';
 import { requestWrapper } from '../../../middlewares';
 import { getCouponByName } from './controller';
 
 const router = Router();
 
-router.get('/coupon/:name', [requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
-  const result = await getCouponByName(req.params.name);
+router.get('/coupon/:name/:environment', [requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
+  const result = await getCouponByName(req.params.name, req.params.environment);
 
   if(result) {
     res
