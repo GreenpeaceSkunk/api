a51ff3caf60646e8bd41128fbfa0b2b6f782ca0b
Dan Tovbein <dtovbein@gmail.com>
2022-02-02 12:23:20 -0300

    Build code

--- dist/routes/v1/application/controller.js
+++ dist/routes/v1/application/controller.js
@@ -18,9 +18,14 @@ const fs_1 = __importDefault(require("fs"));
 const yaml_1 = __importDefault(require("yaml"));
 const getCouponByName = (name, environment) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const content = yield yaml_1.default.parse(fs_1.default.readFileSync(`${path_1.default.resolve('src')}/data/application/coupon/${name}/content.yaml`, 'utf-8'));
-        const settings = yield yaml_1.default.parse(fs_1.default.readFileSync(`${path_1.default.resolve('src')}/data/application/coupon/${name}/settings.yaml`, 'utf-8'));
-        return Promise.resolve(Object.assign({ content: content.data }, { settings: settings.data[environment] }));
+        console.log(`${path_1.default.resolve('src')}/data/application/coupon/${name}.yaml`);
+        const { data } = yield yaml_1.default.parse(fs_1.default.readFileSync(`${path_1.default.resolve('src')}/data/application/coupon/${name}.yaml`, 'utf-8'));
+        console.log(data);
+        // const content = await YAML.parse(fs.readFileSync(`${path.resolve('src')}/data/application/coupon/${name}/content.yaml`, 'utf-8'));
+        // const settings = await YAML.parse(fs.readFileSync(`${path.resolve('src')}/data/application/coupon/${name}/settings.yaml`, 'utf-8'));
+        // return Promise.resolve({...{content: content.data}, ...{settings: settings.data[environment]}});
+        return Promise.resolve(Object.assign({ content: data.content }, { settings: data.settings[environment] }));
+        return Promise.resolve(data);
     }
     catch (error) {
         return Promise.resolve(null);
--- dist/routes/v1/application/index.js
+++ dist/routes/v1/application/index.js
@@ -13,8 +13,9 @@ const express_1 = require("express");
 const middlewares_1 = require("../../../middlewares");
 const controller_1 = require("./controller");
 const router = express_1.Router();
-router.get('/coupon/:name/:environment', [middlewares_1.requestWrapper((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-        const result = yield controller_1.getCouponByName(req.params.name, req.params.environment);
+router.get('/coupon/:name', [middlewares_1.requestWrapper((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+        console.log(req.query.env);
+        const result = yield controller_1.getCouponByName(req.params.name, `${req.query.env}`);
         if (result) {
             res
                 .status(200)
