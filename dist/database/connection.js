283bb5993109bf807d7c9c2e00ee7864db1c3ab0
Dan Tovbein <dtovbein@gmail.com>
2022-02-16 13:11:53 -0300

    Build code

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -9,48 +9,44 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// module.exports = {
+//   connectDatabase: async () => {
+//     console.log(`Connecting to database ${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+//     try {
+//       const db = await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false,
+//         useCreateIndex: true
+//       });
+//       db.connection.on('error', (error: any) => {
+//         console.log("Error", error);
+//       });
+//       db.connection.once('open', (error: any) => {
+//         console.log('Database connected');
+//       });
+//     } catch(error) {
+//       console.log(error);
+//     }
+//   }
+// }
 exports.default = module.exports = () => __awaiter(void 0, void 0, void 0, function* () {
-    // const URI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority&ssl=true`
-    const URI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority&ssl=false`;
-    console.log(`Connecting to database ${URI}`);
-    // DB_USERNAME=danto
-    // DB_PASSWORD=danto
-    // DB_HOST=localhost
-    // DB_PORT=27019
-    // DB_NAME=greenpeace_adn
-    // SECRET_KEY=Gr33np34ce
-    // const mongoose = require('mongoose')
-    try {
-        // const db = await mongoose.connect(`${URI}`, {
-        //   useNewUrlParser: true,
-        //   useUnifiedTopology: true,
-        //   useFindAndModify: false,
-        //   useCreateIndex: true
-        // });
-        // const noteSchema = new mongoose.Schema({
-        //   content: String,
-        //   date: Date,
-        //   important: Boolean,
-        // });
-        // const Note = mongoose.model('Note', noteSchema)
-        // const note = new Note({
-        //   content: 'HTML is Easy',
-        //   date: new Date(),
-        //   important: true,
-        // });
-        // note.save().then((result: any) => {
-        //   console.log('note saved!')
-        //   mongoose.connection.close()
-        // });
-        // db.connection.on('error', (error: any) => {
-        //   console.log("Error", error);
-        // });
-        // db.connection.once('open', (error: any) => {
-        //   console.log('Database connected');
-        // });
-    }
-    catch (error) {
-        console.log(error);
-    }
+    console.log(`Connecting to database ${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+    // try {
+    //   const db = await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    //     useFindAndModify: false,
+    //     useCreateIndex: true
+    //   });
+    //   db.connection.on('error', (error: any) => {
+    //     console.log("Error", error);
+    //   });
+    //   db.connection.once('open', (error: any) => {
+    //     console.log('Database connected');
+    //   });
+    // } catch(error) {
+    //   console.log(error);
+    // }
 });
 //# sourceMappingURL=connection.js.map
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,57 +1,48 @@
 import mongoose = require('mongoose');
 
+// module.exports = {
+//   connectDatabase: async () => {
+//     console.log(`Connecting to database ${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+//     try {
+//       const db = await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false,
+//         useCreateIndex: true
+//       });
+
+//       db.connection.on('error', (error: any) => {
+//         console.log("Error", error);
+//       });
+
+//       db.connection.once('open', (error: any) => {
+//         console.log('Database connected');
+//       });
+//     } catch(error) {
+//       console.log(error);
+//     }
+//   }
+// }
+
 export default module.exports = async () => {
-  // const URI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority&ssl=true`
-  const URI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority&ssl=false`
-  console.log(`Connecting to database ${URI}`);
+  console.log(`Connecting to database ${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+  // try {
+  //   const db = await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //     useFindAndModify: false,
+  //     useCreateIndex: true
+  //   });
 
-  // DB_USERNAME=danto
-  // DB_PASSWORD=danto
-  // DB_HOST=localhost
-  // DB_PORT=27019
-  // DB_NAME=greenpeace_adn
-  // SECRET_KEY=Gr33np34ce
-  // const mongoose = require('mongoose')
+  //   db.connection.on('error', (error: any) => {
+  //     console.log("Error", error);
+  //   });
 
+  //   db.connection.once('open', (error: any) => {
+  //     console.log('Database connected');
+  //   });
 
-  
-  try {
-    // const db = await mongoose.connect(`${URI}`, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useFindAndModify: false,
-    //   useCreateIndex: true
-    // });
-
-    // const noteSchema = new mongoose.Schema({
-    //   content: String,
-    //   date: Date,
-    //   important: Boolean,
-    // });
-    
-    // const Note = mongoose.model('Note', noteSchema)
-    
-    // const note = new Note({
-    //   content: 'HTML is Easy',
-    //   date: new Date(),
-    //   important: true,
-    // });
-    
-    // note.save().then((result: any) => {
-    //   console.log('note saved!')
-    //   mongoose.connection.close()
-    // });
-
-    // db.connection.on('error', (error: any) => {
-    //   console.log("Error", error);
-    // });
-
-    // db.connection.once('open', (error: any) => {
-    //   console.log('Database connected');
-    // });
-
-  } catch(error) {
-    console.log(error);
-  }
-
+  // } catch(error) {
+  //   console.log(error);
+  // }
 }
