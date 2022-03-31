dd7da1f72684c1b4b3f4544933fd2416aa80943f
Dan Tovbein <dtovbein@gmail.com>
2022-03-31 09:15:06 -0300

    Delete dist directory

--- dist/routes/v1/user/model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// import {model} from 'mongoose';
-// import {schema} from './schema';
-// import {IUserDocument} from 'greenpeace';
-// export const Model = model<IUserDocument>('user', schema);
-//# sourceMappingURL=model.js.map
