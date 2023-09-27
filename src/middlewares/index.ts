b93dcc6a558d64e1f08c5b42e83b6854b7be7119
Dan Tovbein <dtovbein@gmail.com>
2023-09-27 15:28:11 -0300

    Hardcode status response

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -11,7 +11,8 @@ const requestWrapper: RequestWrapperType = (fn: (...args: any[]) => void | Promi
     const fnReturn = await fn(req, res, next);
     return fnReturn;
   } catch(e: any) {
-    const status = (e.response.status) ? e.response.status : 500;
+    // const status = (e.response.status) ? e.response.status : 500;
+    const status = 500;
     const errorMessage = (e.response.statusText) ? e.response.statusText : 'Internal Server Error';
     res
       .status(status)
