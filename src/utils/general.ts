87a2323eb02a94bdc7dfcdcdd7ca9ffab65a97b5
Dan Tovbein <dtovbein@gmail.com>
2024-02-11 23:38:12 -0300

    Fix referfer middleware

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -4,6 +4,11 @@ export const getCountryByReferer = (referer = ''): DomainType | undefined => {
   if((referer.match(/\.ar\//) || referer.match(/\.ar/) || '').length) {
     return 'ar';
   }
+  
+  // Remove this afterwards
+  if((referer.match(/\.org\//) || referer.match(/\.org/) || '').length) {
+    return 'ar';
+  }
 
   if((referer.match(/\.co\//) || referer.match(/\.co/) || '').length) {
     return 'co';
