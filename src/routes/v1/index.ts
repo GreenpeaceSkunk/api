0fcac6564e2a975a3679a375480961feb716f0be
Dan Tovbein <dtovbein@gmail.com>
2024-07-03 08:32:45 -0300

    Add payment gateway routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,6 +4,7 @@ import applicationRoutes from './application';
 import locationRoutes from './location';
 import formaRoutes from './forma';
 import campaignRoutes from './campaign';
+import paymentgatewayRoutes from './payment-gateway';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/application', applicationRoutes);
 router.use('/location', locationRoutes);
 router.use('/forma', formaRoutes);
 router.use('/campaign', campaignRoutes);
+router.use('/payment-gateway', paymentgatewayRoutes);
 
 export default router;
