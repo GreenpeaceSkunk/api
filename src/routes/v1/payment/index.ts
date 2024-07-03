102196006b1b881e565fb7199d29d2a4c248f921
Dan Tovbein <dtovbein@gmail.com>
2024-07-03 08:19:14 -0300

    Remove payment route

--- src/routes/v1/payment/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import transbankRoutes from './transbank';
-
-const router = Router();
-
-router.use('/tranbsbank', transbankRoutes);
-
-export default router;
