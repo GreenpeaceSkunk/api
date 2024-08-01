0ef73dcfd17ca41fd9fda33be3add264558ea023
Dan Tovbein <dtovbein@gmail.com>
2024-08-01 16:43:23 -0300

    Fix conlifcts

--- src/routes/v1/payment-gateway/transbank/index.ts
+++ src/routes/v1/payment-gateway/transbank/index.ts
@@ -1,30 +1,4 @@
 import { Router, Request, Response, NextFunction } from 'express';
-<<<<<<< Updated upstream
-import { refererWrapper, requestWrapper } from '../../../../middlewares';
-import { create } from './conroller';
-
-const router = Router();
-
-/**
- * Ref: https://www.transbankdevelopers.cl/referencia/oneclick#crear-una-inscripcion
- */
-router.get('/create', [refererWrapper, requestWrapper(async (req: Request, res: Response, next: NextFunction) => {
-  // const response = await create();
-  // try {
-    // const {body, params: { paymentGateway }} = req;
-    // const result = await postRecord(formId, body, req.header('Referer'));
-
-    res
-      // .status(result.status)
-      .status(200)
-      .json({});
-  // } catch (error: any) {
-  //   res
-  //     .status(500)
-  //     .json({ errorMessage: 'Error when posting new record into ForMa' });
-  // }
-})]);
-=======
 import { requestWrapper } from '../../../../middlewares';
 import { create, confirm } from './controller';
 
@@ -40,6 +14,5 @@ router.post('/create', [
 //   res.json(await confirm(req.body));
 // })]);
 router.post('/confirm', [requestWrapper(confirm)]);
->>>>>>> Stashed changes
 
 export default router;
